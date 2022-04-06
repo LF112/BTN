44ad4914afea8914104363bca035b069df9b0cfc
futiwolf <lf@lf112.net>
2022-04-06 21:22:56 +0800

    ⚡️ 优化主动 Fetch 请求

--- src/state/fetch/hooks.ts
+++ src/state/fetch/hooks.ts
@@ -1,6 +1,7 @@
 import { useCallback } from 'react'
 import { AppState } from 'state'
 import { useAppDispatch, useAppSelector } from 'state/hooks'
+import { updateStatus } from 'state/status/slice'
 
 import { DateTime } from 'luxon'
 import axios from 'axios'
@@ -16,54 +17,85 @@ import qs from 'qs'
 
 /**
  * Default fetch | '站点安排的网络请求任务罢了。'
- * @param slice 要更新的 state slice
  * @param url URL 地址，不含有域名等
  * @param type 请求类型，默认为 get
- * @param polling 轮询时间，默认为 0 则不开启
  * @param param 参数，默认为 {}
  * @param options 额外参数，默认为 {}
  */
-export function $fetch(
-	slice: any,
+export function BTFetch(): (
 	url: string,
-	type = 'get',
-	polling = 0,
-	param = {} as any,
-	options = {} as any
-): () => void {
+	type?: string,
+	param?: any,
+	options?: any
+) => Promise<void> {
 	//=> 读取 CONFIG
 	const {
 		connect: { baseUrl, apiUse, apiKey }
 	} = useAppSelector((state: AppState) => state.config /* state name */)
 
-	//=> 使用 API 模式时
-	if (apiUse) {
-		const time = DateTime.now().toFormat('X')
-		const format: number = JSON.parse(time)
-		param.request_token = MD5(`${format}${MD5(apiKey)}`).toString()
-		param.request_time = format
-	}
-	//=> 解决跨域
-	if (!options.headers) options.headers = {}
-	options.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+	const dispatch = useAppDispatch()
 
 	//=> Main
-	//=> READ STATE
-	const dispatch = useAppDispatch()
-	if (!polling && polling === 0)
-		//=> 仅为请求时
+	return useCallback(
+		async (url, type = 'post', param = {}, options = {}) => {
+			//=> 使用 API 模式时
+			if (apiUse) {
+				const time = DateTime.now().toFormat('X')
+				const format: number = JSON.parse(time)
+				//=> 签名 API 密钥 | '理论上签名一次即可，宝塔面板不需要每次请求重新签名，后期可进行缓存优化。'
+				param.request_token = MD5(`${format}${MD5(apiKey)}`).toString()
+				param.request_time = format
+			}
 
-		return useCallback(
-			() =>
-				axios[apiUse ? 'post' : type](url, qs.stringify(param), {
+			//=> SET HEADER
+			if (!options.headers) options.headers = {}
+			//=> 解决跨域 ?
+			options.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+			const [err, result] = await axios[apiUse ? 'post' : type](
+				url,
+				qs.stringify(param),
+				{
 					baseURL: baseUrl,
 					...options
-				})
-					.then((result: any) => dispatch(slice(result.data)))
-					.catch((error: any) => dispatch(slice(error))),
-			[dispatch]
-		)
-	else {
-		//=> 轮询
-	}
+				}
+			)
+				.then((data: any) => [null, data])
+				.catch((err: any) => [err, null])
+
+			if (!err) {
+				const { data } = result
+				const { status, msg } = data
+
+				//=> API 请求失败校验
+				if (!status && msg)
+					if (/验证失败,禁止|IP校验失败,您的访问/g.test(msg)) {
+						dispatch(
+							updateStatus({
+								data: false,
+								type: 'network',
+								aims: 'apiStatus',
+								rawJson: data,
+								aimsJson: 'msg'
+							})
+						)
+						return null
+					}
+
+				//=> 打回数据
+				return data
+			} else {
+				dispatch(
+					updateStatus({
+						data: false,
+						type: 'network',
+						aims: 'apiStatus',
+						rawJson: { status: false, msg: err.message },
+						aimsJson: 'msg'
+					})
+				)
+				return { status: false, msg: err.message }
+			}
+		},
+		[dispatch]
+	)
 }
