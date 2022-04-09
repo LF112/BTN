af6e0f9490104476bc1f9e15a189c7c8a41ae41c
futiwolf <lf@lf112.net>
2022-04-10 02:01:22 +0800

    💄 修复数据图表样式溢出

--- src/components/page/console/DataHub/index.tsx
+++ src/components/page/console/DataHub/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import styled from 'styled-components'
 //[ package ]
 
 import Network from './Network'
 import Iostat from './Iostat'
+import OverflowMask from 'components/reusable/Mask/Overflow'
 //[ component ]
 
 import { useApiState } from 'state/api/hooks'
@@ -16,22 +17,42 @@ import { bytesToSize } from 'utils/useTools'
 export default () => {
 	const { up, down, upTotal, downTotal, iostat } = useApiState('system')
 
+	//=> 显示遮罩
+	const cardBoxNode = useRef<HTMLDivElement>(null)
+	const [showMask, updateShowMask] = useState<boolean>(false)
+	useEffect(() => {
+		if (cardBoxNode) {
+			const DOM = cardBoxNode.current
+			if (DOM.scrollHeight > DOM.clientHeight) updateShowMask(!showMask)
+		}
+	}, [cardBoxNode])
+
 	return (
-		<Main>
-			<Network
-				up={up}
-				down={down}
-				upTotal={bytesToSize(upTotal)}
-				downTotal={bytesToSize(downTotal)}
-			/>
-			<Iostat data={iostat} />
+		<Main showMask={showMask}>
+			<nav ref={cardBoxNode}>
+				<Network
+					up={up}
+					down={down}
+					upTotal={bytesToSize(upTotal)}
+					downTotal={bytesToSize(downTotal)}
+				/>
+				<Iostat data={iostat} />
+			</nav>
+			<OverflowMask showMask={showMask} bottom={true} />
 		</Main>
 	)
 }
 
 //=> Style
-const Main = styled.main`
+const Main = styled.main<{ showMask: boolean }>`
+	position: relative;
 	width: 100%;
 	height: calc(100% - 202px);
-	overflow-y: auto;
+	overflow: auto;
+	> nav {
+		width: 100%;
+		height: 100%;
+		overflow-y: auto;
+		${(props: any) => (props.showMask ? 'padding-bottom: 30px;' : '')}
+	}
 `
