bf80f8903a0e0d3b58008dd6253bd6b8d3cf1a52
futiwolf <lf@lf112.net>
2022-05-01 23:25:00 +0800

    🍻 更改控制台徽章为常驻

--- src/main.tsx
+++ src/main.tsx
@@ -29,13 +29,11 @@ createRoot(document.getElementById('BTNEXT')).render(
 )
 
 //=> 控制台徽章
-if (process.env.NODE_ENV === 'production') {
-	console.clear()
-	console.log(
-		'\n %c \u26a1BT NEXT %c https://www.lf112.net %c BY%c LF112  \n\n',
-		'color: #ffffff; background: rgb(0, 145, 228); padding:5px 0;',
-		'background:rgba(197, 197, 197, 0.89); padding:5px 0;',
-		'color: #ffffff; background: rgba(49, 49, 49, 0.85); padding:5px 0;',
-		'color: rgb(0, 145, 228); background: rgba(49, 49, 49, 0.85); padding:5px 0;'
-	)
-}
+if (process.env.NODE_ENV === 'production') console.clear()
+console.log(
+	'\n %c \u26a1BT NEXT %c https://www.lf112.net %c BY%c LF112  \n\n',
+	'color: #ffffff; background: rgb(0, 145, 228); padding:5px 0;',
+	'background:rgba(197, 197, 197, 0.89); padding:5px 0;',
+	'color: #ffffff; background: rgba(49, 49, 49, 0.85); padding:5px 0;',
+	'color: rgb(0, 145, 228); background: rgba(49, 49, 49, 0.85); padding:5px 0;'
+)
