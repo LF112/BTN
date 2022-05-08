c62a2e5dff1265f815667ea27f4091546a533517
futiwolf <lf@lf112.net>
2022-05-08 09:51:31 +0800

    ✨ 新增可复用设置条目

---
+++ src/components/page/config/ReusableComponents.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+//[ package ]
+
+import { DefaultCard } from 'components/reusable/Card'
+//[ components ]
+
+export const SetUpMain = styled(DefaultCard)`
+	width: 100%;
+	padding: 15px 10px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	> div {
+		width: 342px;
+		display: flex;
+		align-items: center;
+	}
+`
+
+export const SetUpTitle = styled.h2`
+	color: #fff;
+	font-size: 15px;
+	line-height: 1;
+	font-family: 'HarmonyOS';
+	user-select: none;
+	margin: 0 15px;
+`
+
+export const SetUpDescription = styled.p`
+	color: #6e788e;
+	font-family: 'HarmonyOS';
+	font-size: 14px;
+	font-weight: bold;
+	line-height: 1.2;
+`
