bb98b95225b20bba27c40f814628e0a3bcf95bcd
LF112 <lf@lf112.net>
2022-06-25 19:19:16 +0800

    ✨ 更新控制台徽章

--- src/main.tsx
+++ src/main.tsx
@@ -37,9 +37,9 @@ createRoot(document.getElementById('BTNEXT')).render(
 //=> 控制台徽章
 if (process.env.NODE_ENV === 'production') console.clear()
 console.log(
-	'\n %c \u26a1BT NEXT %c https://www.lf112.net %c BY%c LF112  \n\n',
-	'color: #ffffff; background: rgb(0, 145, 228); padding:5px 0;',
-	'background:rgba(197, 197, 197, 0.89); padding:5px 0;',
+	'\n %c \u26a1BT NEXT  %c https://www.lf112.net %c  BY %cLF112 \n\n',
+	'color: #fff; background: rgb(0, 145, 228); padding:5px 0;border-radius: 4px 0 0 4px',
+	'background:#323842; padding:5px 0;',
 	'color: #ffffff; background: rgba(49, 49, 49, 0.85); padding:5px 0;',
-	'color: rgb(0, 145, 228); background: rgba(49, 49, 49, 0.85); padding:5px 0;'
+	'color: rgb(0, 145, 228); background: rgba(49, 49, 49, 0.85); padding:5px 0;border-radius: 0 4px 4px 0'
 )
